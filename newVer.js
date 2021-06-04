631caa375d662c21137dbb9467ae060f503b8a68
Dmitrij <s.d144@yandex.by>
2021-06-04 21:33:55 +0300

    work func declaration

--- newVer.js
+++ newVer.js
@@ -9,53 +9,96 @@
 // учитываются имена параметров при вызове объявленной функции
 
 
+
+
+// объект для хранения глобальных переменных и функций
+
+
+
+
+const globalObj = {};
+
+// объект с базовыми операциями и функциями
+const dispatcher = {
+	'+': (...args) => args.reduce((prev, curr) => prev + curr),
+	'*': (...args) => args.reduce((prev, curr) => prev * curr),
+	'-': (...args) => args.reduce((prev, curr) => prev - curr),
+
+	'defn': (...args) => defn(...args),
+	'princ': (...args) => console.log(...args),
+	'defvar': (name, value) => globalObj[name] = value,
+
+	//ect basics operators and functions
+}
+
+// const defn = (functionName, args, body) => {
+// 	// требуется реализация
+// 	const [fn, ...fnArgs] = body;
+
+// 	const arrangeArgs = (unorderedArgs) =>
+// 		fnArgs.map((a) => unorderedArgs[args.indexOf(a)]);
+
+// 	globalObj[functionName] = (...args) => fn(...arrangeArgs(args))
+// };
+
+
+
+
+
+
+// const parseBody = (expression) => {
+
+// 	const [operator, ...exp] = expression;
+
+// 	exp.map((each) => {
+// 		if (globalObj[each]) {
+// 			return globalObj[each]
+// 		}
+// 	})
+// }
+
 const defn = (functionName, args, body) => {
 
+	const [operation, ...rest] = body
 
+	globalObj[functionName] = (...calledArguments) => {
+
+
+
+		// const newArgs = rest.map((a) => calledArguments[args.indexOf(a)]);
+
+
+		const newArgs = rest.map((a) => {
+			console.log(a)
+			if (calledArguments[args.indexOf(a)]) {
+				return calledArguments[args.indexOf(a)]
+			}
+			return checkGlobalObject(a)
+
+
+
+		})
+
+
+
+		console.log(newArgs)
+		// const params = rest.map((ar) => ar[args.indexOf(ar)])
+
+		return checkGlobalObject(operation).apply(dispatcher, newArgs)
+	}
 
 }
 
 
+const calculate = (statement) => {
 
+	const [operation, ...rest] = statement
 
-
-
-const interpret = (...code) => {
-	// объект для хранения глобальных переменных и функций
-	const globalObj = {};
-
-	// объект с базовыми операциями и функциями
-	const dispatcher = {
-		'+': (...args) => args.reduce((prev, curr) => prev + curr),
-		'*': (...args) => args.reduce((prev, curr) => prev * curr),
-		'-': (...args) => args.reduce((prev, curr) => prev - curr),
-
-		'defn': (...args) => defn(...args),
-		'princ': (...args) => console.log(...args),
-		'defvar': (name, value) => globalObj[name] = value,
-
-		//ect basics operators and functions
-	}
-
-	const checkGlobalObject = (arg) => {
-
-		if (globalObj[arg]) {
-			return globalObj[arg]
-		} else if (dispatcher[arg]) {
-			return dispatcher[arg];
-		} else {
-			return arg
-		}
-	}
-
-
-	const calculate = (statement) => {
-
-		const [operation, ...rest] = statement
-
+	if (operation === 'defn') {
+		checkGlobalObject(operation).apply(dispatcher, rest)
+	} else {
 		const mapped = rest.map((el) => {
 			if (typeof el !== 'object') {
-				// console.log('element')
 				return checkGlobalObject(el)
 			} else {
 				return calculate(el)
@@ -63,9 +106,30 @@ const interpret = (...code) => {
 		})
 
 		const res = checkGlobalObject(operation).apply(dispatcher, mapped);
+
+		// console.log(res)
 		return res
 	}
 
+
+
+}
+
+const checkGlobalObject = (arg) => {
+
+	if (globalObj[arg]) {
+		return globalObj[arg]
+	} else if (dispatcher[arg]) {
+		return dispatcher[arg];
+	} else {
+		return arg
+	}
+}
+
+
+
+
+const interpret = (...code) => {
 
 	code.forEach((statement) => {
 
@@ -74,29 +138,34 @@ const interpret = (...code) => {
 	})
 }
 
+
+
+
+
 // // Функция, используемая в runtime
 // const sum = (...args) => args.reduce((prev, curr) => prev + curr)
 
 
-
-defn('sum', ['a', 'b', 'c'], ['a' + 'b' + 'c'])
-
-
 // // Пример вызова функции interpret cо всеми возмонжостями (они неограничены (шутка))
 const result = interpret(
-	['princ', 'Hello World,'],
-	['+', 10, 20, 30, ['-', 1, 2, ['*', 2, 2]]],
-	['defvar', 'x', 5],
-	['defvar', 'y', 4],
-	['princ', 'x', 'y'],
-	['princ', ['+', 'x', 1]],
-	['princ', ['+', 'x', 'y']],
-	['princ', ['+', 10, 20, 30, ['-', 1, 'x', 2, ['*', 2, 2]]]] // need more brackets!
+	// ['princ', 'Hello World,'],
+	// ['+', 10, 20, 30, ['-', 1, 2, ['*', 2, 2]]],
+	// ['defvar', 'x', 5],
+	// ['defvar', 'y', 4],
+	// ['princ', 'x', 'y'],
+	// ['princ', ['+', 'x', 1]],
+	// ['princ', ['+', 'x', 'y']],
+	// ['princ', ['+', 10, 20, 30, ['-', 1, 'x', 2, ['*', 2, 2]]]], // need more brackets!
 
-	// // [defn, "sum3", ['a', 'b', 'c'], [sum, 'a', 'b', 'c']],
-	// // ['sum3', 10, 20, 30],
+	// ['defn', "sum3", ['a', 'b', 'c'], ['+', 'c', 'b', 'x']],  // x глобальная переменная, определена выше
+	// ['princ', ['sum3', 10, 20, 30]],  // cuz c=30, b=20, x=5
+	// ['defn', 'subtract', ['a', 'b', 'c'], ['-', 'c', 'b', 'a']],
+	// ['princ', ['subtract', 1, 2, 3]],
+
+	// ['sum3', 10, 20, 30],
 
 )
 
+// console.log(globalObj.sum3(3, 7, 5))  
 // console.log(result)
 // console.assert(result === 60)
