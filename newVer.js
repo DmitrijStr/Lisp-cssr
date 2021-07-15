bb58a0fb73c828a8e3496f43d78a2da8aa613602
Dmitrij <s.d144@yandex.by>
2021-07-15 19:44:19 +0300

    init

--- newVer.js
+++ newVer.js
@@ -1,171 +1,99 @@
-/*	Этот простой интерператор имеет возможность выполнения некоторых арифмитически операций. Пример ['*', 2, 2], ['+', 2, ['*', 2, 2]];
- *	Имеет встроенные функции вывода на экран, объявления функций и переменных;
- *	Расширяемость достигается за счёт добавления методов в объект dispatcher;
- *  объявление переменных с помощью ключевого слова defvar;
- *  объявление функций с помощью ключевого слова defn;
- *  вывод в консоль с помощью функции princ;
+/* Этот простой интерператор имеет возможность выполнения некоторых арифмитически операций.
+   Пример ['*', 2, 2], ['+', 2, ['*', 2, 2]];
+ * Имеет встроенные функции вывода на экран, объявления функций и переменных;
+ * Расширяемость достигается за счёт добавления методов в объект dispatcher;
+ * объявление глобальных переменных с помощью ключевого слова defvar;
+ * объявление функций с помощью ключевого слова defn;
+ * вывод в консоль с помощью функции princ;
+ * учитываются имена параметров при вызове объявленной функции;
  */
 
-// учитываются имена параметров при вызове объявленной функции
-
-
-
-
 // объект для хранения глобальных переменных и функций
+const globalObj = {
+  'sum3': (...calledArguments) => checkList(body, args, calledArguments),
 
 
+};
 
-
-const globalObj = {};
-
-// объект с базовыми операциями и функциями
+// объект с базовыми операциями и функциями использующимеся в runtime
 const dispatcher = {
-	'+': (...args) => args.reduce((prev, curr) => prev + curr),
-	'*': (...args) => args.reduce((prev, curr) => prev * curr),
-	'-': (...args) => args.reduce((prev, curr) => prev - curr),
+  '+': (...args) => args.reduce((prev, curr) => prev + curr),
+  '*': (...args) => args.reduce((prev, curr) => prev * curr),
+  '-': (...args) => args.reduce((prev, curr) => prev - curr),
 
-	'defn': (...args) => defn(...args),
-	'princ': (...args) => console.log(...args),
-	'defvar': (name, value) => globalObj[name] = value,
+  defn: (...args) => defn(...args),
+  princ: (...args) => console.log(...args),
+  defvar: (name, value) => { globalObj[name] = value; },
+  // ect basics operators and functions
+};
 
-	//ect basics operators and functions
-}
+const lookEnviroment = (arg) => {
+  if (globalObj[arg]) {
+    return globalObj[arg];
+  } if (dispatcher[arg]) {
+    return dispatcher[arg];
+  } return arg;
+};
 
-// const defn = (functionName, args, body) => {
-// 	// требуется реализация
-// 	const [fn, ...fnArgs] = body;
+const checkList = (body, argList = [], params = []) => {
+  const [operation, ...rest] = body;
+  if (operation === 'defn') { // guard exp for defn func
+    lookEnviroment(operation).apply(dispatcher, rest);
+  } else {
+    const parsedArgs = rest.map((a) => {
+      if (params[argList.indexOf(a)]) {
+        return params[argList.indexOf(a)];
+      }
+      if (typeof a === 'object') {
+        return checkList(a, argList, params);
+      }
+      return lookEnviroment(a);
+    });
 
-// 	const arrangeArgs = (unorderedArgs) =>
-// 		fnArgs.map((a) => unorderedArgs[args.indexOf(a)]);
-
-// 	globalObj[functionName] = (...args) => fn(...arrangeArgs(args))
-// };
-
-
-
-
-
-
-// const parseBody = (expression) => {
-
-// 	const [operator, ...exp] = expression;
-
-// 	exp.map((each) => {
-// 		if (globalObj[each]) {
-// 			return globalObj[each]
-// 		}
-// 	})
-// }
+    return lookEnviroment(operation).apply(dispatcher, parsedArgs);
+  }
+};
 
 const defn = (functionName, args, body) => {
-
-	const [operation, ...rest] = body
-
-	globalObj[functionName] = (...calledArguments) => {
-
-
-
-		// const newArgs = rest.map((a) => calledArguments[args.indexOf(a)]);
-
-
-		const newArgs = rest.map((a) => {
-			console.log(a)
-			if (calledArguments[args.indexOf(a)]) {
-				return calledArguments[args.indexOf(a)]
-			}
-			return checkGlobalObject(a)
-
-
-
-		})
-
-
-
-		console.log(newArgs)
-		// const params = rest.map((ar) => ar[args.indexOf(ar)])
-
-		return checkGlobalObject(operation).apply(dispatcher, newArgs)
-	}
-
-}
-
-
-const calculate = (statement) => {
-
-	const [operation, ...rest] = statement
-
-	if (operation === 'defn') {
-		checkGlobalObject(operation).apply(dispatcher, rest)
-	} else {
-		const mapped = rest.map((el) => {
-			if (typeof el !== 'object') {
-				return checkGlobalObject(el)
-			} else {
-				return calculate(el)
-			}
-		})
-
-		const res = checkGlobalObject(operation).apply(dispatcher, mapped);
-
-		// console.log(res)
-		return res
-	}
-
-
-
-}
-
-const checkGlobalObject = (arg) => {
-
-	if (globalObj[arg]) {
-		return globalObj[arg]
-	} else if (dispatcher[arg]) {
-		return dispatcher[arg];
-	} else {
-		return arg
-	}
-}
-
-
-
+  globalObj[functionName] = (...calledArguments) => checkList(body, args, calledArguments);
+};
 
 const interpret = (...code) => {
-
-	code.forEach((statement) => {
-
-		calculate(statement)
-
-	})
-}
-
-
-
-
+  code.forEach((list) => checkList(list));
+};
 
 // // Функция, используемая в runtime
 // const sum = (...args) => args.reduce((prev, curr) => prev + curr)
 
-
-// // Пример вызова функции interpret cо всеми возмонжостями (они неограничены (шутка))
+// // Пример вызова функции interpret cо всеми возмонжостями
 const result = interpret(
-	// ['princ', 'Hello World,'],
-	// ['+', 10, 20, 30, ['-', 1, 2, ['*', 2, 2]]],
-	// ['defvar', 'x', 5],
-	// ['defvar', 'y', 4],
-	// ['princ', 'x', 'y'],
-	// ['princ', ['+', 'x', 1]],
-	// ['princ', ['+', 'x', 'y']],
-	// ['princ', ['+', 10, 20, 30, ['-', 1, 'x', 2, ['*', 2, 2]]]], // need more brackets!
+  //   ['princ', 'Hello World,'], // пример: вывод на экран
+  //   ['+', 10, 20, 30, ['-', 1, 2, ['*', 2, 2]]], // пример: базовые арифметические операции
+  ['defvar', 'x', 5], // пример: объявление переменных
+  //   ['defvar', 'y', 4],
+  //   ['princ', 'x', 'y'], // пример: вывод значений переменных на экран
+  //   ['princ', ['+', 'x', 1]],
+  //   ['princ', ['+', 'x', 'y']],
+  //   ['princ', ['+', 10, 20, 30, ['-', 1, 'x', 2, ['*', 2, 2]]]], // need more brackets!
+  //   // работа с функциями
+  //   ['defn', 'sum3', ['a', 'b', 'c'], ['+', 'c', 'b', 'x']], // пример: объявление функции. x глобальная переменная, определена выше
+  //   ['princ', ['sum3', 10, 20, 30]], // пример: вызов функции.
+  //   ['defn', 'subtract', ['a', 'b', 'c'], ['-', 'c', 'b', 'a']],
+  //   ['princ', ['subtract', 1, 2, ['sum3', 10, 20, 'y']]], // c - b - a. a- результат вып. функции sum3
+  //   ['defn', 'someFunc', ['a', 'b', 'c', 'd', 'e', 'f'], ['+', 'x', 'c', 'b', 'd', ['-', 'a', 'e', ['*', 'f', 'b']]]],
+  //   ['princ', ['someFunc', 1, 2, 3, 4, 5, 6]],
 
-	// ['defn', "sum3", ['a', 'b', 'c'], ['+', 'c', 'b', 'x']],  // x глобальная переменная, определена выше
-	// ['princ', ['sum3', 10, 20, 30]],  // cuz c=30, b=20, x=5
-	// ['defn', 'subtract', ['a', 'b', 'c'], ['-', 'c', 'b', 'a']],
-	// ['princ', ['subtract', 1, 2, 3]],
+  // - Не учитываются имена параметров при вызове объявленной функции
 
-	// ['sum3', 10, 20, 30],
+  ['defn', 'subtract2', ['a', 'b', 'c'], ['-', 'a', 'b', 'c']],
+  // ['defn', 'subtract3', ['a', 'b', 'c'], ['-', 'c', 'b', 'a']],
 
-)
+  ['subtract2', 5, 4, 'x'],
+  // ['princ', ['subtract3', 5, 4, 3]],
 
-// console.log(globalObj.sum3(3, 7, 5))  
-// console.log(result)
-// console.assert(result === 60)
+  // если бы не учитывались имена параметров то мы бы ожидали всегда a-b-c
+  // фактически в качестве параметра можно передать любое выражение( теоретически ),
+  // которое в момент вызова будет высчитано.
+  // ['princ', ['subtract3', 5, 4, 'x']],  когда значение x будет высчитано, результат будет присвоен c
+  // или я не понял того, что мы ожидаем от интерпетатора?
+);
